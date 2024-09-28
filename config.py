30399e6d71c8820877351d1246740bdafd1ebb77
jeremy <100848946+JeremyOmwenga@users.noreply.github.com>
2024-09-28 23:32:50 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+import requests
+class config():
+    NEWS_API_URL = "https://mboum-finance.p.rapidapi.com/v1/markets/news"
+    HISTORY_API_URL = "https://mboum-finance.p.rapidapi.com/v1/markets/stock/history"
+    
+
+    headers = {
+        "X-RapidAPI-Key": "ccb3bbe154msh0b42c25c74afe49p148485jsn6ea61b8b81e6",
+        "X-RapidAPI-Host": "mboum-finance.p.rapidapi.com"
+    }
